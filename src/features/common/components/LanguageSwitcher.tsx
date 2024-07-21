e05dfeb08d15da48e02261d8935589aabaf838b2
Lê Huy Vũ <huyvu@evol.vn>
2024-07-21 15:52:12 +0700

    Adding language switcher

---
+++ src/features/common/components/LanguageSwitcher.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Trans } from "react-i18next";
+import { FunctionComponent } from "react";
+import { useParams } from "next/navigation";
+import { useTranslation } from "@/i18n/client";
+import { fallbackLng, languages } from "@/i18n/settings";
+import Link from "next/link";
+
+type LanguageSwitcherProps = {};
+
+const LanguageSwitcher: FunctionComponent<LanguageSwitcherProps> = () => {
+  const params = useParams();
+
+  const lng = params?.lng ?? fallbackLng;
+
+  const { t } = useTranslation("common");
+
+  return (
+    <div
+      className={`
+        w-full flex items-center justify-center p-3
+        text-sm text-white fixed z-50 left-0 bottom-0
+      `}
+    >
+      <Trans
+        t={t}
+        parent={"span"}
+        values={{ lng }}
+        i18nKey={"language_switcher"}
+        components={[<strong key={"language_switcher"} />]}
+      />
+
+      <ul className={'flex px-1 items-center gap-x-1'}>
+        {languages
+          .filter((lang) => lang !== lng)
+          .map((lang) => (
+            <li key={`language_${lang}`}>
+              <Link href={`/${lang}`}>{lang}</Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default LanguageSwitcher;
